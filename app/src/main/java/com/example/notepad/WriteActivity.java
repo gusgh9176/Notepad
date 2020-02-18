2da8767a5851f874bddc2d22247b2a51dc8f7ca6
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-02-18 18:50:39 +0900

    Add 사진 추가 키
    추가
    WriteActivity 액션바의 사진 추가 키 추가

--- app/src/main/java/com/example/notepad/WriteActivity.java
+++ app/src/main/java/com/example/notepad/WriteActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -94,6 +95,14 @@ public class WriteActivity extends AppCompatActivity {
         }
 
     };
+
+    // 액션버튼 메뉴 액션바에 집어 넣기
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.write_menu, menu);
+        return true;
+    }
+
     // 액션버튼을 클릭했을때의 동작
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
