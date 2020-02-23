c5516c84972b7b5c8d34bb446278862bc5b516af
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-02-23 13:49:32 +0900

    Chage DetailActivity 정리
    변경
    코드 위치, 주석 추가, 전역변수 지역변수로 바꾸기

--- app/src/main/java/com/example/notepad/DetailActivity.java
+++ app/src/main/java/com/example/notepad/DetailActivity.java
@@ -22,15 +22,17 @@ import com.example.notepad.vo.ImageVO;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private TextView tvTitle;
-    private TextView tvNotepadNumber;
-    private TextView tvDescription;
     private String key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView tvTitle;
+        TextView tvNotepadNumber;
+        TextView tvDescription;
+        int imageOrder = 0; // 메모의 몇번째 이미지인지 판단하는 숫자
 
         // 액션바 설정하기 //
         // 액션바 타이틀 변경하기
@@ -41,35 +43,42 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
         // 액션바 설정 끝
 
+        // 뷰 변수에 받아오기
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvNotepadNumber = (TextView) findViewById(R.id.tvNotepadNumber);
         tvDescription = (TextView) findViewById(R.id.tvDescription);
 
+        // MainActivity에서 클릭한 메모의 DB key 값 받아옴
         Intent intent = getIntent();
         key = intent.getStringExtra("key");
 
-        DetailNotepadVO articleVO = NoteDB.getNotepad(key);
-        int imageOrder = 0;
+        // 받아온 key 값 통해서 DB에서 해당 메모에 대한 내용(제목, 본문), 이미지 받아옴
+        // key 값 = NoteDB Map<String, DetailNotepadVO> db 변수의 key, 메모를 구분해줌
+        // key + imageOrder = ImageDB Map<String, ImageVO> db 변수의 key, 앞선 key로 어느 메모에 있는 이미지인지 판단하고 imageOrder 통해 메모 내부의 이미지 구분해줌
+        DetailNotepadVO detailNotepadVO = NoteDB.getNotepad(key);
         ImageVO imageVO = ImageDB.getImage(key + imageOrder);
-        String title = articleVO.getTitleStr();
-        String notepadNumber = Integer.toString(articleVO.getNotepadNo());
-        String description = articleVO.getDescription();
 
+        // 받아온 내용 변수에 저장 후 화면에 세팅
+        String title = detailNotepadVO.getTitleStr();
+        String notepadNumber = Integer.toString(detailNotepadVO.getNotepadNo());
+        String description = detailNotepadVO.getDescription();
         tvTitle.setText(title);
         tvNotepadNumber.setText(notepadNumber);
         tvDescription.setText(description);
 
-
+        // 메모에 이미지가 있다면 반복문 진행함
         while (imageVO != null) {
-            if(!imageVO.isDelete()) { // 존재함
+            if(!imageVO.isDelete()) { // 이미지가 삭제 작업을 받은 이미지가 아닌 경우 화면에 세팅
                 setImage(imageVO.getImageUrl());
             }
+            // 메모의 다음 이미지를 불러오기 위해 imageOrder의 값을 1 늘려주고 새로 이미지를 얻어옴
             imageOrder++;
             imageVO = ImageDB.getImage(key + imageOrder);
         }
 
     }
 
+    // 이미지를 표시하는 레이아웃에 임시 imageView 객체 만들어서 Glide 함수 통하여 이미지뷰를 추가해줌
     private void setImage(String path) {
         LinearLayout li = (LinearLayout) findViewById(R.id.detailPicList);
         ImageView imageView = new AppCompatImageView(this);
@@ -79,22 +88,24 @@ public class DetailActivity extends AppCompatActivity {
         li.addView(imageView);
     }
 
-    // 액션버튼을 클릭했을때의 동작
+    // actionbar의 버튼을 눌렀을때의 동작
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home: // actionbar의 back 키 눌렀을 때 동작
+                // 현재 DetailActivity를 종료 시키고, 이전 Activity(MainActivity)로 이동시켜줌
                 finish();
                 break;
-            case R.id.action_edit: // edit 키 눌렀을 때 WriteActivity로 key 담은 intent 전달
-                // TODO : use key data.
-                // WriteActivity index 값 전달
+            case R.id.action_edit: // actionbar의 edit 키 눌렀을 때 동작
+                // 기존에 있던 메모 편집
+                // key를 담아서 WriteActivity를 실행시키고 DetailActivity는 종료
                 Intent intent = new Intent(DetailActivity.this, WriteActivity.class);
                 intent.putExtra("key", key);
                 startActivity(intent);
                 finish();
                 break;
             case R.id.action_delete: // actionbar의 delete 키 눌렀을 때 동작
+                // 각 DB 객체에 들어있는 db 변수의 내부 데이터 값(여기선 delete 변수)을 변경하여 삭제된 데이터 판단을 함
                 NoteDB.getNotepad(key).setDelete(true); // NoteDB의 delete 변수를 true로 만들어 삭제된 데이터라고 알림
                 NoteDB.save(getFilesDir());
                 Toast.makeText(this, "해당 메모가 삭제되었습니다.", Toast.LENGTH_SHORT).show();
